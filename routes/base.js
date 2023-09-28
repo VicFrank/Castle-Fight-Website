4d47a2b4e8049ff1dfb2aff5bb3eb383d44b7003
VicFrank <matthewshattil@gmail.com>
2023-09-27 23:10:13 -0500

    updated packages

--- routes/base.js
+++ routes/base.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const players = require("../db/players");
-const games = require("../db/games");
-const test = require("../test");
 const apicache = require("apicache");
+const cache = apicache.middleware;
 
 router.get("/", async (req, res) => {
-  // games.create(test.sampleGameData);
   res.json({ info: "API for Castle Fight stats" });
 });
-
-// router.get("/test", async (req, res) => {
-//   try {
-//     const gameID = games.create(test.sampleGameData);
-//     res.json(gameID);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({ message: "Server Error" });
-//   }
-// });
 
 router.get("/leaderboard", cache("5 minutes"), async (req, res) => {
   try {
